b5fe644abd1bf8170a3790be3332e395e68f6cf9
iLnazhim <bass_boss@mail.ru>
2023-01-19 15:24:35 +0300

    first commit

---
+++ pages/product_page.py
@@ -0,0 +1,151 @@
+import time
+from selenium.common import StaleElementReferenceException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+from pages.data import DataPage
+from utilities.logger import Logger
+
+import allure
+from base.base_class import BaseClass
+
+
+class ProductPage(BaseClass):
+
+    # Locators
+    button_town = "//div[@class='notification__city js-popup-city-tgl js-notification-city-confirm']"
+    input_town = "//input[@placeholder='Ваш город']"
+    choose_town = "//li[@class='popup-city__item js-city-item'][1]"
+    accept_cookie = "//div[@class='policy-banner__close js-coockie-banner-button']"
+    button_reserve = "//button[@class='btn btn_small map-item__btn map-item__choose js-select-shop js-reserve-shop']"
+    button_cart = "//a[@class='header-cart']"
+    button_make_reserve = "//button[@class='btn p-basket-item__btn js-popup-reserve-tgl js-reserve-send']"
+    input_name = "//input[@placeholder='Ваше имя']"
+    input_phone = "//input[@name='customer_phone']"
+    input_mail = "//input[@name='customer_email']"
+    checkbox = "//label[@class='small checkbox__label']"
+    button_make_order = "//input[@class='btn popup-form__submit']"
+    button_ok = "//button[@class='btn popup__btn js-popup-reserve-tgl']"
+
+
+    # Getters
+    def get_button_town(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_town)))
+
+    def get_input_town(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.input_town)))
+
+    def get_choose_town(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.choose_town)))
+
+    def get_accept_cookie(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.accept_cookie)))
+
+    def get_button_reserve(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_reserve)))
+
+    def get_button_cart(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_cart)))
+
+    def get_button_make_reserve(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_make_reserve)))
+
+    def get_input_name(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.input_name)))
+
+    def get_input_phone(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.input_phone)))
+
+    def get_input_mail(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.input_mail)))
+
+    def get_checkbox(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.checkbox)))
+
+    def get_button_make_order(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_make_order)))
+
+    def get_button_ok(self):
+        return WebDriverWait(self.browser, 30).until(EC.element_to_be_clickable((By.XPATH, self.button_ok)))
+
+    # Actions
+    def click_button_town(self):
+        self.get_button_town().click()
+        print("Click button_town")
+
+    def input_input_town(self):
+        self.get_input_town().send_keys(*DataPage.town)
+        print("Input town")
+
+    def click_choose_town(self):
+        self.get_choose_town().click()
+        print("Click choose_town")
+
+    def click_accept_cookie(self):
+        self.get_accept_cookie().click()
+        print("Click accept_cookie")
+
+    def click_button_reserve(self):
+        self.get_button_reserve().click()
+        print("Click_button_reserve")
+
+    def click_button_cart(self):
+        self.get_button_cart().click()
+        print("Click_button_cart")
+
+    def click_button_make_reserve(self):
+        self.get_button_make_reserve().click()
+        print("button_make_reserve")
+
+    def input_input_name(self):
+        self.get_input_name().send_keys(*DataPage.name)
+        print("input_name")
+
+    def input_input_phone(self):
+        self.get_input_phone().send_keys(*DataPage.phone)
+        print("input_phone")
+
+    def input_input_mail(self):
+        self.get_input_mail().send_keys(*DataPage.email)
+        print("input_mail")
+
+    def click_checkbox(self):
+        self.get_checkbox().click()
+        print("Click checkbox")
+
+    def click_button_make_order(self):
+        self.get_button_make_order().click()
+        print("Click button_make_order")
+
+
+    def click_button_ok(self):
+        self.get_button_ok().click()
+        print("Click button_ok")
+
+
+    # Metods
+    def make_the_order(self):
+        """make_the_order"""
+        with allure.step("make_the_order from product page"):
+            Logger.add_start_step(method="make_the_order")
+
+            self.get_current_url()
+            self.click_button_town()
+            self.input_input_town()
+            time.sleep(3)
+            self.click_choose_town()
+            self.click_accept_cookie()
+            self.click_button_reserve()
+            self.click_button_cart()
+            self.click_button_make_reserve()
+            self.input_input_name()
+            self.input_input_phone()
+            self.input_input_mail()
+            self.click_checkbox()
+            self.click_button_make_order()
+            self.click_button_ok()
+            Logger.add_end_step(url=self.browser.current_url, method="make_the_order")
+
+
+
---
+++ tests/test_make_order.py
@@ -0,0 +1,22 @@
+import time
+import allure
+
+from pages.product_page import ProductPage
+
+
+
+@allure.description("Test order_from_product_page")
+def test_order_from_product_page(browser):
+
+    """Ссылка на тестовый"""
+    # link = "https://grechka:digital@xiaomi-shop.grechka.digital/catalog/smartfony-i-planshety/redmi/redmi-10c-3-64-gb-mint-green"
+    """Ссылка на боевой"""
+    link = "https://xiaomi-stores.ru/catalog/smartfony-i-planshety/redmi/redmi-10c-3-64-gb-mint-green"
+    print("\nStart Order Test")
+    browser.maximize_window()
+    pp = ProductPage(browser, link)
+    pp.open()
+    pp.make_the_order()
+
+    print("Finish Tests")
+    time.sleep(1)
